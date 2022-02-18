7858aeeb1949fff20d91d5e3d45ae04b71fb0a52
evanmalherbe <evanmalherbe@gmail.com>
2022-02-18 10:11:52 +0200

    Updated index.js to try fix heroku deploy error

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ app.use(bodyParser.json());
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
-app.use(express.static(path.join(__dirname, "public")));
+//app.use(express.static(path.join(__dirname, "public")));
+
+app.use(express.static(path.resolve(__dirname, "frontend/build")));
 
 // Set path to .env file
 dotenv.config({ path: ".env" });
@@ -85,12 +87,17 @@ mongoose.connection.on("error", (error) => {
   }
 });
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "frontend/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "frontend/build")));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+//   });
+// }
+
+// All remaining requests return the React app, so it can handle routing.
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "frontend/build", "index.html"));
+});
 
 // Set port number
 const PORT = process.env.PORT || 8001;
